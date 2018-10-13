b55f5e076c90fd425d90e4de10b0f6f68c2bba60
路一直都在 <1692916750@qq.com>
2018-10-13 10:20:59 +0800

    some python code

--- hardway.py
+++ hardway.py
@@ -28,3 +28,62 @@ def f4():
             c1.write(content)
 
 f4()
+class exercise:
+    def break_words(self,sentence):
+        return sentence.split(" ")
+    def sort_words(self,sentence):
+        return sorted(sentence)
+    def first(self,sentence):
+        return sentence.pop(0)
+    def last(self,sentence):
+        return sentence.pop(-1)
+
+def f5():
+    import random
+    num=random.randint(1,100)
+    n=0
+
+    while(1):
+        n+=1
+        guess = int(input("please guess a number between(1,100)>>"))
+        if(guess>num):
+            print("your guess is too large,try a smaller one~")
+            continue
+        elif(guess<num):
+            print("your guess is too small,try a larger one~")
+            continue
+        else:
+            print('Bingo! the answer is %d !'%num)
+            break
+    print("用了{0}次机会".format(n))
+def f6():
+    shopping=['apple','banana','orange']
+    blankt=[]
+    for i in shopping:
+        print('I have bought %s'%i)
+        blankt.append(i)
+    print('look at my blankt:')
+    for i in range(len(blankt)):
+
+        print('%s'%blankt[i])
+
+def f7():
+    f=open("e:/copy.txt",'r')
+    n=len(f.readlines())
+    print(n)
+f7()
+class Thing(object):
+    def test(self,hi):
+        print('hi')
+a=Thing()
+a.test('hello')
+def f8():
+    ten_things='Apples Oranges Crows Telephone Light Sugar'
+    stuff=ten_things.split(' ')
+    print(stuff)
+    more_stuff=['Day','Night','Song','Banana']
+    while(len(stuff)!=10):
+        next_one=more_stuff.pop()
+        print('Adding:',next_one)
+        stuff.append(next_one)
+f8()
